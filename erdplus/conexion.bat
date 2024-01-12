5141122c9e8c03778f4d49c136441e4029c09ffa
DiEgOfercri <dfc25@icloud.com>
2024-01-12 11:41:53 +0100

    conexion

--- erdplus/conexion.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd C:\Diurno\modelos\erdplus
-sqlplus usuariodb/Azarquiel2023@S1DAMW2023_high
---
+++ sql/join.sql
@@ -0,0 +1,4 @@
+UPDATE prueba_a
+SET columna_a_actualizar = nuevo_valor
+WHERE condicion_columna = 'valor_especifico';
+/
